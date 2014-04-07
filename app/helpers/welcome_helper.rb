27481781f48cfaec98591adf2a0b6e79efb68b11
Aleksey Leontiev <i@aleontiev.me>
2014-04-07 20:27:31 +0400

    zerofy

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -12,9 +12,9 @@ module WelcomeHelper
   end
 
   def quantity(record)
-    record.mahabig || 0+ 
-    record.big || 0 + 
-    record.medium || 0 + 
-    record.small || 0
+    (record.mahabig || 0) + 
+    (record.big || 0) + 
+    (record.medium || 0) + 
+    (record.small || 0)
   end
 end
