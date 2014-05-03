154cf02479dc1e467ca56dd1525d94cfbc6bf52a
Aleksey Leontiev <i@aleontiev.me>
2014-05-03 13:10:30 +0400

    location permalink

--- app/admin/location.rb
+++ app/admin/location.rb
@@ -1,3 +1,3 @@
 ActiveAdmin.register Location do
-  permit_params :name, :email, :short_name
+  permit_params :name, :email, :short_name, :permalink
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,12 @@
 class Location < ActiveRecord::Base
   has_many :locations
+  before_create :set_permalink
+
+  def set_permalink
+  	puts transliterate(self.name)
+    self.permalink = ActiveSupport::Inflector.transliterate(self.name)
+      .downcase
+      .gsub(/[^0-9a-z ]/i, "")
+      .gsub(" ", "-")
+  end
 end
